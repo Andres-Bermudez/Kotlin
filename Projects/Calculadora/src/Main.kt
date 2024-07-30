d7956b860f68658c9ec419aa83e7da804d0385f2
Andres-Bermudez <Andres201900@outlook.com>
2024-07-30 09:13:07 -0500

    Aprendiendo Lambdas

--- Projects/Calculadora/src/Main.kt
+++ Projects/Calculadora/src/Main.kt
@@ -13,7 +13,11 @@
 
 private var primervalor = 0.0
 private var segundoValor = 0.0
-private var resultado = 0.0
+
+val sumar = {a : Double, b : Double -> a + b}
+val restar = {a : Double, b : Double -> a - b}
+val multiplicar = {a : Double, b : Double -> a * b}
+val dividir = {a : Double, b : Double -> a / b}
 
 fun main() {
     var entradaUsuario = 0
@@ -25,56 +29,38 @@ fun main() {
         println("   2. Restar")
         println("   3. Multiplicar")
         println("   4. Dividir")
+        println("   0. Salir")
         print("Ingresa tu opcion: ")
 
         try {
             entradaUsuario = readln().toInt()
-            tomarDatos()
+
+            if (entradaUsuario == 0) {
+                System.exit(0)
+            }
+
+            print("Ingresa el primer valor: ")
+            primervalor = readln().toDouble()
+
+            print("Ingresa el segundo valor: ")
+            segundoValor = readln().toDouble()
+
             println()
 
             when (entradaUsuario) {
-                1 -> sumar()
-                2 -> restar()
-                3 -> multiplicar()
-                4 -> dividir()
+                1 -> println("El resultado de la suma es: ${sumar(primervalor, segundoValor)}")
+                2 -> println("El resultado de la resta es: ${restar(primervalor, segundoValor)}")
+                3 -> println("El resultado de la multiplicacion es: ${multiplicar(primervalor, segundoValor)}")
+                4 -> if (segundoValor.toInt() == 0) {
+                    println("Imposible dividir entre 0")
+                } else {
+                    println("El resultado de la division es: ${dividir(primervalor, segundoValor)}")
+                }
                 else -> println("¡Opcion no disponible!")
             }
         } catch (e: NumberFormatException) {
             println()
             println("¡Solo puedes elegir entre las opciones disponibles!")
         }
-    }
-}
-
-private fun tomarDatos() {
-    print("Ingresa el primer valor: ")
-    primervalor = readln().toDouble()
-
-    print("Ingresa el segundo valor: ")
-    segundoValor = readln().toDouble()
-}
-
-private fun sumar() {
-    resultado = primervalor.plus(segundoValor)
-    println("El resultado de la suma es: $resultado")
-}
-
-private fun restar() {
-    resultado = primervalor.minus(segundoValor)
-    println("El resultado de la resta es: $resultado")
-}
-
-private fun multiplicar() {
-    resultado = primervalor.times(segundoValor)
-    println("El resultado de la multiplicacion es: $resultado")
-}
-
-private fun dividir() {
-    if (segundoValor.toInt() == 0) {
-        println("¡No es posible dividir entre 0!")
-
-    } else {
-        resultado = primervalor.div(segundoValor)
-        println("El resultado de la division es: $resultado")
     }
 }
